9075514c61b977ca3f5279db91630eab535a9bd3
dhis2mis-maker <dhis2mis@gmail.com>
2026-05-26 09:16:41 +0200

    fix studio config

--- app/studio/[[...index]]/page.tsx
+++ app/studio/[[...index]]/page.tsx
@@ -1,7 +1,20 @@
 'use client'
 
 import { NextStudio } from 'next-sanity/studio'
-import config from '../../../sanity.config'
+import { defineConfig } from 'sanity'
+import { deskTool } from 'sanity/desk'
+import { schemaTypes } from '../../../schemas'
+
+const config = defineConfig({
+  name: 'hive-luxury',
+  title: 'The Hive Luxury Management',
+  projectId: 'cjyjqtda',
+  dataset: 'production',
+  plugins: [deskTool()],
+  schema: {
+    types: schemaTypes,
+  },
+})
 
 export default function StudioPage() {
   return <NextStudio config={config} />
